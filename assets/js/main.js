d497651c3c2f4a06cabbcd4e2b0bec65f30476f3
Abdulaha Islam <abdulahaislam210917@gmail.com>
2021-09-12 13:02:53 +0600

    Almost Done.
    Header top, Header bottom, Header search, Product cart, footer, footer bottom.
    
    Responsive not checked!

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,24 @@
             $ (".navigation, .off-canvas-overlay").removeClass ("toggled");
         });
 
+        $('#example').hierarchySelect({
+            hierarchy: false,
+            width: 'auto'
+        });
+
+          $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+              $('.scroll-to-top').fadeIn();
+            }
+            else {
+              $('.scroll-to-top').fadeOut();
+            }
+          });
+
+        $(".scroll-to-top").click(function() {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
         
     });
 
